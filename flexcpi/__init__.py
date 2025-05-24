aaf6546dc78f19cea890ce3253b229fc58596a13
Rory Quinlan <72473095+RoryQo@users.noreply.github.com>
2025-05-23 20:24:27 -0400

    Create __init__.py

---
+++ flexcpi/__init__.py
@@ -0,0 +1,8 @@
+from .flexcpi_package_module import (
+    load_catalog_tables,
+    load_weight_tables,
+    match_series_ids_to_weights,
+    fetch_cpi_series_data,
+    compute_custom_cpi_index,
+    plot_custom_cpi
+)
